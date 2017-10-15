83c7392c7dfd2066e688d006f631307e577107a4
Donald Disha <Donald Disha>
2017-10-15 19:55:25 -0400

    reading

--- ex.c
+++ ex.c
@@ -4,56 +4,73 @@
 #include <string.h>
 
 int main(){
-  int welcomeSocket, newSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
+  int sockfd, clientlen, portno, n, newSocket;
+  char buffer[256];
+  struct sockaddr_in serverAddr, clientAddr;
   struct sockaddr_storage serverStorage;
+
   socklen_t addr_size;
 
   /*---- Create the socket. The three arguments are: ----*/
   /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+  sockfd = socket(PF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) 
+    error("ERROR opening socket");
   
   /*---- Configure settings of the server address struct ----*/
   /* Address family = Internet */
   serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  serverAddr.sin_port = htons(7891);
+
+
   /* Set port number, using htons function to use proper byte order */
 
   /* Set IP address to localhost */
-  int len = 20;
-  char str[len];
-  inet_ntop( AF_INET, &serverAddr.sin_addr, str, len);  
-  printf("IP address is: %s\n", str); 
- 
-//  inet_ntoa(serverAddr.sin_addr.s_addr));
-  
+  // int len = 20;
+  // char str[len];
+  // inet_ntop( AF_INET, &serverAddr.sin_addr, str, len);  
+  // printf("IP address is: %s\n", str); 
+  //  inet_ntoa(serverAddr.sin_addr.s_addr));
   // SET IP ADDRESS
-  serverAddr.sin_addr.s_addr = htons(INADDR_ANY);
-  // serverAddr.sin_addr.s_addr = inet_addr("172.31.18.149");
-
   // SET PORT 
-  serverAddr.sin_port = htons(7891);
-
- //printf("IP address is: %s\n", inet_ntoa(serverAddr.sin_addr.s_addr));
+  //printf("IP address is: %s\n", inet_ntoa(serverAddr.sin_addr.s_addr));
   /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+  // memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
   /*ddress is: %s\n", inet_ntoa(client_addr.sin_addr));---- Bind the address struct to the socket ----*/
-  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+  if (bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)
+    error("ERROR on binding");
+  
+  bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
   /*---- Listen on the socket, with 5 max connection requests queued ----*/
-  if(listen(welcomeSocket,5)==0)
+  if(listen(sockfd, 5) == 0)
     printf("Listening\n");
   else
     printf("Error\n");
 
   /*---- Accept call creates a new socket for the incoming connection ----*/
-  addr_size = sizeof serverStorage;
-  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+  clientlen = sizeof clientAddr;
+  newSocket = accept(sockfd, (struct sockaddr *) &clientAddr, &clientlen);
+
+  if (newsockfd < 0) 
+    error("ERROR on accept")
+
+  bzero(buffer,256);
+  n = read(newSocket, buffer, 255);
+  
+  if (n < 0) 
+    error("ERROR reading from socket");
+
+  printf("Here is the message: %s\n",buffer);
 
   /*---- Send message to the socket of the incoming connection ----*/
   strcpy(buffer,"Hello World\n");
-  send(newSocket,buffer,13,0);
+
+  n = write(newSocket,buffer, 13);
+  if (n < 0) 
+    error("ERROR writing to socket");
 
   return 0;
 }
